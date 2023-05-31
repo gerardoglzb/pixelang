0195cbf3fbe41cefae577b14dff28db964711d39
Gerardo Gonzalez Becerril <gerbecdev@gmail.com>
2023-05-31 16:33:09 -0600

    Librerías

--- include/ImageProcessing/Image.hpp
+++ include/ImageProcessing/Image.hpp
@@ -1,5 +1,7 @@
-#include <iostream>
-using namespace std;
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 struct Image {
     uint8_t *data = NULL;
